b5092d124fb63d9efba4dc8ed25efb0cde611dcc
NorAtl <w0136969@campus.nscc.ca>
2023-10-30 11:38:56 -0300

    working with functions

--- Python/PRACTICE/Oct 30/leapYear.py
+++ Python/PRACTICE/Oct 30/leapYear.py
@@ -8,27 +8,33 @@
 #determine if an inputted year is a leap year
 #leap year == divisible by 4 but not 100
 
-    
+def checkLeapYear(in_year):
+    isLeapYear = False
+    if(in_year % 4 == 0 and in_year % 100 != 0)or(in_year % 400 == 0):
+        isLeapYear = True
+    return isLeapYear
 
-def main(): #<-- Don't change this line!
-    #Write your code below. It must be indented!
+def main():
 
     print('\nLeap Year calculator\n')
-
     year = int(input('Please enter a year: '))
 
-    if year/4 == int(year/4) and year/100 != int(year/100):
-        print('This is a leap year.')
-    else: 
-        print('This is not a leap year.')
+    if checkLeapYear(year):
+        print(f'{year} is a leap year.')
+    else:
+        print(f'{year} is not a leap year.')
 
-
+# Without using a function:
+#    if year/4 == int(year/4) and year/100 != int(year/100):
+#        print('This is a leap year.')
+#    else: 
+#        print('This is not a leap year.')
+#
+# Another way to write this is:
+#   if (year % 4 == 0 and year % 100 !+ 0) or (year % 400 == 0):
+#       print('This is a leap year.')
+#    else: 
+#        print('This is not a leap year.')
   
-
-
-
-    #Your code ends on the line above
-
-#Do not change any of the code below!
 if __name__ == "__main__":
     main()
