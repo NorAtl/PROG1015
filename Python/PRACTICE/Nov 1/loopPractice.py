f34e6401a13da05143075ddf67fea3f730baa1a6
NorAtl <w0136969@campus.nscc.ca>
2023-11-01 11:10:57 -0300

    commit

---
+++ Python/PRACTICE/Nov 1/loopPractice.py
@@ -0,0 +1,21 @@
+
+#Don't forget to rename this file after copying the template
+#for a new program!
+# """
+# Student Name:    
+# Program Title:  
+# Description:    
+# """
+
+def main(): #<-- Don't change this line!
+    #Write your code below. It must be indented!
+
+    for counter in range(10):  #don't have to use 'counter': you can use any variable name
+        print('message')        #range(10) means 0,1,2,3,4,5,6,7,8,9
+
+
+    #Your code ends on the line above
+
+#Do not change any of the code below!
+if __name__ == "__main__":
+    main()
---
+++ Python/PRACTICE/Nov 1/turtlePractice.py
@@ -0,0 +1,36 @@
+#Don't forget to rename this file after copying the template
+#for a new program!
+# """
+# Student Name:    
+# Program Title:  
+# Description:    
+# """
+
+import turtle
+
+def draw():
+    turtle.forward(100)
+    turtle.right(90)
+
+def main():
+
+    for square in range(4): #0,1,2,3
+        draw()
+
+    # turtle.forward(100)
+    # turtle.right(90)
+
+    # turtle.forward(100)
+    # turtle.right(90)
+
+    # turtle.forward(100)
+    # turtle.right(90)
+
+    # turtle.forward(100)
+    # turtle.right(90)
+
+    # turtle.done()
+
+
+if __name__ == "__main__":
+    main()
